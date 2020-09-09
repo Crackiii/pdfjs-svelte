600bc463b337aac01e33a7b627d2939e1d1a9aa9
Nadeem Ahmad <antesoft17@outlook.com>
2020-09-09 20:12:38 +0500

    PDF JS with viewer

---
+++ src/pdf-viewer/new.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { onMount } from "svelte";
+</script>
+
+<style>
+  :global(body) {
+    margin: 0;
+    padding: 0;
+  }
+  .iframe {
+    width: 100vw;
+    height: 100vh;
+    border: none;
+  }
+</style>
+
+<iframe
+  src="./pdfjs-dist/web/viewer.html?file=ford.pdf"
+  title="vidy"
+  class="iframe" />
