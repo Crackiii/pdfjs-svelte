8588fc73ce1904c73835c9d0474c91a6d748a761
Nadeem Ahmad <antesoft17@outlook.com>
2020-09-09 22:31:40 +0500

    Initial embeds on pdf

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,8 @@
 <script>
   // import PdfViewer from "./pdf-viewer/viewer.svelte";
   import Viewer from "./pdf-viewer/new.svelte";
-  const src =
-    new URLSearchParams(location.search).get("src") || "./Nadeem_Ahmad_U.pdf";
+  // const src =
+  //   new URLSearchParams(location.search).get("src") || "./Nadeem_Ahmad_U.pdf";
 </script>
 
 <!-- <PdfViewer {src} /> -->
